e501a12e1523c10c42dc4c23e73537421e4ce06e
ShrekBenson <120696828+ShrekBenson@users.noreply.github.com>
2024-07-17 20:06:24 -0600

    Set option icons

--- src/js/components/side-menu.js
+++ src/js/components/side-menu.js
@@ -5,11 +5,13 @@ export default function icons() {
   icons.forEach((icon, index) => {
     const img = new Image();
     img.src = icon;
+    //img.width = 25;
   
     const iconName = requireIcons.keys()[index].replace('./', '').replace('.svg', '');
       
-    const menuItem = document.querySelector(iconName);
-    if (menuItem) {
+    const menuItem = document.querySelector(`.${iconName.trim()}`);
+
+    if (menuItem) {      
       menuItem.appendChild(img);
     }
   });  
